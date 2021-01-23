eb2c73280ec4a7f0f8d891c465f0800412a239c6
Luiz Sodre <luizsodrerj@gmail.com>
2021-01-23 20:11:30 -0300

    exemplo fluxo bytes

---
+++ java standard/src/fluxobytes/ApuracaoVotosArquivo.java
@@ -0,0 +1,63 @@
+package fluxobytes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ApuracaoVotosArquivo {
+
+	public static void main(String[] args) {
+		ApuracaoVotosArquivo apuracao = new ApuracaoVotosArquivo();
+		
+		apuracao.apurarVotos();
+	}
+
+	void apurarVotos() {
+		try {
+			List<Candidato>listaCandidatos = new ArrayList<>(); 
+					
+			File arquivo = new File("C:\\Users\\beto\\Desktop\\Mentoria & Cursos\\treinamento\\java standard\\votacao.txt");
+			BufferedReader buffer = new BufferedReader(new FileReader(arquivo));
+			
+			String linhaArquivo = buffer.readLine();
+			
+			while (linhaArquivo != null) {
+				StringTokenizer tokenizer = new StringTokenizer(linhaArquivo, ",");
+				
+				if (tokenizer.hasMoreTokens()) {
+					String nome  = tokenizer.nextToken();
+					int votos    = Integer.parseInt(tokenizer.nextToken().trim());
+					
+					Candidato candidato = new Candidato(nome, votos);
+					listaCandidatos.add(candidato);
+				}
+				linhaArquivo = buffer.readLine();	
+			}
+			buffer.close();
+			
+			for (Candidato candidato : listaCandidatos) {
+				System.out.println(
+					candidato.nome + " - " + 
+					candidato.totalVotos + " votos"
+				);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ java standard/src/fluxobytes/Candidato.java
@@ -0,0 +1,22 @@
+package fluxobytes;
+
+public class Candidato {
+
+	int totalVotos;
+
+	String nome;
+
+	
+	
+	public Candidato(String nome, int totalVotos) {
+		super();
+		this.totalVotos = totalVotos;
+		this.nome = nome;
+	}
+
+
+	public Candidato() {
+	}
+	
+	
+}
