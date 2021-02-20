f75ab0ea731ca3884ce69bb093cd0fa39ddea05e
Luiz Sodre <luizsodrerj@gmail.com>
2021-02-20 20:49:01 -0300

    bkp exercicios prontos para gravação

---
+++ bkp - standard/jdbc/ecommerce/Produto.java
@@ -0,0 +1,42 @@
+package jdbc.ecommerce;
+
+public class Produto {
+
+	private Pedido pedido;
+	private String descricao;
+	private Integer qtdPedida;
+	private Double preco;
+	
+	
+	public Double getValorTotal() {
+		return qtdPedida * preco;
+	}
+	
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Integer getQtdPedida() {
+		return qtdPedida;
+	}
+	public void setQtdPedida(Integer qtdPedida) {
+		this.qtdPedida = qtdPedida;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}
